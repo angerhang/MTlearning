7462d3d1c7687b64aeaea43c86afad1e33d088d0
angerhang <514140809@qq.com>
2017-07-12 17:23:48 +0200

    Predict using baseline model

---
+++ subject_predict.m
@@ -0,0 +1,65 @@
+function [ predictions ] = subject_predict(subject_topredict, order_idx, ...
+                     model_all_bands_bp, original_information_struct_am)
+% takes a subject id and train on the other subjects
+% after being optimized on the first 20 trials, returns the predictions
+% The data input format: for the linear model we have 
+% data in the form of features * labels. In this test data
+% we have 24 by 300. We can understand for each trial we have
+% 24 features, and we have 300 trials with 5 subjects. 
+% For the bilinear model, the input is of the form 
+% electrodes * features * labels, so the label here can be considered 
+% as the trial label in a reocrding sessionn.
+% INPUT subject_topredict : the subject id 
+%       order_idx: the covariance index 
+%       model_all_bands_bp: the EEG feature
+%       original_information_struct_am: the data labels 
+
+%% model prep
+fprintf('Processing subject %d ...\n', subject_topredict)
+
+% Instantiate models
+n_its = 10;
+order = {'l2','l2-trace','l1-diag','l1'};
+
+% data spliting 
+% train data 
+subject_to_train = 1:26;
+subject_to_train = subject_to_train(subject_to_train~=subject_topredict);
+train_x = model_all_bands_bp.features.mov(subject_to_train);
+% only select the features after the 20th timestep
+train_y = original_information_struct_am.log_of_narj_jerk_data(subject_to_train);
+
+% individual data for optimization 
+test_x = model_all_bands_bp.features.mov(subject_topredict);
+opt_x = mat2cell(test_x{1}(:, 1:20), 590);
+opt_y = mat2cell(original_information_struct_am. ... 
+                 log_of_narj_jerk_data{subject_topredict}(1:20, :), 20, 1);
+
+% test data 
+test_x = mat2cell(test_x{1}(:, 21:end), 590);
+test_y = mat2cell(model_all_bands_bp.predictions_observations_update_subj_upd20. ... 
+         observations_of_updated{subject_topredict}, 1, size(test_x{1}, 2));
+      
+%% model construction      
+disp(['********************* Covariance update: ', order{order_idx}, ... 
+       '*************************']);
+regression_model = MT_linear_regression('dim_reduce',1,'n_its',1e2, ... 
+              'lambda_ml',0,'cov_flag',order{order_idx},'zero_mean',1);
+disp('Confirm prior computation switches: ');
+regression_model.printswitches;
+
+% Code to fit the prior (training on the first 4)
+disp('Training regression prior...')  
+regression_model.fit_prior(train_x, train_y);
+
+% Code to fit the new task (with cross-validated lambda)
+new_regression = regression_model.fit_new_task(opt_x{1}, opt_y{1},'ml',0);
+
+% Classifying after the new task update
+predictions = new_regression.predict(test_x{1});
+fprintf('rmse on new task for regression model: %.2f\n',  ... 
+    sqrt(mean((predictions - test_y{1}').^2)));
+
+
+end
+

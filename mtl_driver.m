f345b2bbe38cc7b94a7a6a191697812e851ec7f8
angerhang <angerhangy@gmail.com>
2017-08-14 16:59:09 +0200

    Better cluster

--- mtl_driver.m
+++ mtl_driver.m
@@ -1,4 +1,4 @@
-function [sub] = mtl_driver(extract_mode)
+function [sub] = mtl_driver(extract_mode, model_opt)
 %extract_driver triggers the extraction routine 
 % in two modes: the cluster mode or the stand-alone mode.
 %   Input: extract_mode
@@ -6,28 +6,33 @@ function [sub] = mtl_driver(extract_mode)
 %              run on a signle computer. The extraction of each
 %              subject takes around 3 hours and expect this to
 %              take a long time.
-%           2. cluster-mode when script is running on MPI's cluster
+%           2: cluster-mode when script is running on MPI's cluster
 %              the each subject's extraction will be submitted as a job
 %              and all the jobs can be run in paralle but requires one to
 %              be familiar with the cluster operations. 
+%           model_opt
+%           1: the original mtl model 
+%           2: the sparce encoded input with mtl model
+%
+%
 
 n_subject = 26;
 
 if extract_mode == 1
     
     for i=1:n_subject
-        comnorm_predict(1, i);
+        comnorm_predict(model_opt, i);
     end
     
 elseif extract_mode == 2
     addpath('/usr/local/share/htcondor_matlab/');
     htjob = htcondorjob('comnorm_predict');
-    htjob.mem = 4000;
-    % memroy 4GB
+    htjob.mem = 8000;
+    % memroy 8GB
     htjob.bid = 5;
 
     for i=1:n_subject
-        htjob.addJob(2, i);
+        htjob.addJob(model_opt, i);
     end
 
     htjob.run;
